19b37254c2f66072643d56f63e6622c444cd3750
Binit S. <binitsaha91@gmail.com>
2026-05-27 18:09:57 +0530

    Bulk sync: 24 submissions

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>() ; 
+        for (int num : nums){
+            set.add(num);
+        }
+        int res = 0 ; 
+        
+        for (int num : nums ){
+            if ( !set.contains(num-1)){
+                int len = 1 ; 
+                while (set.contains(num + len)){
+                    len ++ ; 
+                }
+                res = Math.max(res,len);
+            }
+        }
+        return res ; 
+    }
+}
